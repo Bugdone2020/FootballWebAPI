becf5a94c227718f8a85b4a656e2332a79bf1763
Bugdone2020 <bohdankozlov@gmail.com>
2023-07-03 22:53:01 +0300

    Add project files.

---
+++ FootballWebAPI/Controllers/FootballerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FootballerController : ControllerBase
+    {
+        private readonly ILogger<FootballerController> _logger;
+
+        public FootballerController(ILogger<FootballerController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+    }
+}
