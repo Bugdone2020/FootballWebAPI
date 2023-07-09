73953f9e4c916692ead7dc867b086b3611cc4b87
Bugdone2020 <bohdankozlov@gmail.com>
2023-07-09 14:27:56 +0300

    Add Client entity

--- DataAccessLayer/EFCoreDbContext.cs
+++ DataAccessLayer/EFCoreDbContext.cs
@@ -6,6 +6,7 @@ namespace DataAccessLayer
     public class EFCoreDbContext : DbContext
     {
         public DbSet<Footballer> Footballers { get; set; }
+        public DbSet<Client> Clients { get; set; }
 
         public EFCoreDbContext(DbContextOptions<EFCoreDbContext> options) : base(options)
         {
